6abcae3e5a83cd6f88df040b50c1cfe4de1263d5
tkgstrator <tkgstrator@users.noreply.github.com>
2024-05-18 09:09:10 +0900

    Initialize develop

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { csrf } from 'hono/csrf'
+import { logger } from 'hono/logger'
+import { cache } from 'hono/cache'
+
+const app = new Hono()
+
+app.use(logger())
+app.use(csrf())
+app.use('*', cors())
+app.onError((err, c) => {
+  console.error(err)
+  return c.text('Internal Server Error', 500)
+})
+
+export default {
+  port: 3000,
+  fetch: app.fetch
+}
